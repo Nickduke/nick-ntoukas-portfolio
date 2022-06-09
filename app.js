f596c53dc40e0ccf696daba829941fb03df240b2
nickNtoukas <ntoukas.nick@gmail.com>
2022-06-09 18:32:00 -0400

    Styled tools section

--- app.js
+++ app.js
@@ -13,57 +13,55 @@ document.querySelectorAll('.nav__links--link').forEach((n) =>
   })
 );
 
-// document.querySelectorAll('.fa-linkedin-in').forEach((i) => {
-//   const linkedinIcon = document.querySelector('.fa-linkedin-in');
-//   const githubIcon = document.querySelector('.fa-github');
-//   const instagramIcon = document.querySelector('.fa-instagram');
-//   const twitterIcon = document.querySelector('.fa-twitter');
-//   const youtubeIcon = document.querySelector('.fa-youtube');
+// ------------------------WORK ON THIS--------------------
+// const bar2 = document.querySelector('.bar2');
+// const bar3 = document.querySelector('.bar3');
 
-//   i.addEventListener('mouseover', () => {
-//     linkedinIcon.classList.remove('dark');
-//   });
-//   i.addEventListener('mouseout', () => {
-//     linkedinIcon.classList.add('dark');
-//   });
+// hamburger.addEventListener('mouseover', () => {
+//   bar2.classList.add('bar2-effect');
+//   bar3.classList.add('bar3-effect');
+// });
+// hamburger.addEventListener('mouseout', () => {
+//   bar2.classList.remove('bar2-effect');
+//   bar3.classList.remove('bar3-effect');
 // });
 
 const linkedinIcon = document.getElementById('linkedin');
-linkedinIcon.addEventListener('mouseover', () => {
-  document.querySelector('.fa-linkedin-in').classList.remove('dark');
-});
 linkedinIcon.addEventListener('mouseout', () => {
-  document.querySelector('.fa-linkedin-in').classList.add('dark');
+  document.querySelector('.fa-linkedin-in').classList.remove('linkedin');
+});
+linkedinIcon.addEventListener('mouseover', () => {
+  document.querySelector('.fa-linkedin-in').classList.add('linkedin');
 });
 
 const githubIcon = document.getElementById('github');
-githubIcon.addEventListener('mouseover', () => {
-  document.querySelector('.fa-github').classList.remove('dark');
-});
 githubIcon.addEventListener('mouseout', () => {
-  document.querySelector('.fa-github').classList.add('dark');
+  document.querySelector('.fa-github').classList.remove('green');
+});
+githubIcon.addEventListener('mouseover', () => {
+  document.querySelector('.fa-github').classList.add('green');
 });
 
 const instagramIcon = document.getElementById('instagram');
-instagramIcon.addEventListener('mouseover', () => {
-  document.querySelector('.fa-instagram').classList.remove('dark');
+instagramIcon.addEventListener('mouseout', () => {
+  document.querySelector('.fa-instagram').classList.remove('insta');
 });
-instagram.addEventListener('mouseout', () => {
-  document.querySelector('.fa-instagram').classList.add('dark');
+instagram.addEventListener('mouseover', () => {
+  document.querySelector('.fa-instagram').classList.add('insta');
 });
 
 const twitterIcon = document.getElementById('twitter');
-twitterIcon.addEventListener('mouseover', () => {
-  document.querySelector('.fa-twitter').classList.remove('dark');
-});
 twitterIcon.addEventListener('mouseout', () => {
-  document.querySelector('.fa-twitter').classList.add('dark');
+  document.querySelector('.fa-twitter').classList.remove('blue');
+});
+twitterIcon.addEventListener('mouseover', () => {
+  document.querySelector('.fa-twitter').classList.add('blue');
 });
 
 const youtubeIcon = document.getElementById('youtube');
-youtubeIcon.addEventListener('mouseover', () => {
-  document.querySelector('.fa-youtube').classList.remove('dark');
-});
 youtubeIcon.addEventListener('mouseout', () => {
-  document.querySelector('.fa-youtube').classList.add('dark');
+  document.querySelector('.fa-youtube').classList.remove('red');
+});
+youtubeIcon.addEventListener('mouseover', () => {
+  document.querySelector('.fa-youtube').classList.add('red');
 });
